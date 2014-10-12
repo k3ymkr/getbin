da760a2d9a03942b22c8eb22f0a6bddeeab7b6a8
Thomas Clements <k3ymkr@gmail.com>
2014-10-11 20:27:46 -0400

    Initial commit.  Just wrote this.

---
+++ README.md
@@ -0,0 +1,5 @@
+This script just takes in a file and makes a binary file from it based on the capitalization.  I suppose it's a decent way to encode a binary....probably not.
+
+I mostly did it for the PN18 code.  It didn't get me anywhere though.  After rulling out binary encoded ascii, I thought it might be a binary file.  If it is, I don't know what it is.
+
+I thought the code might be useful for another project someday.
---
+++ bin/genbin.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env py
+
+import re,sys,os,binascii
+if len(sys.argv) < 3:
+	print >>sys.stderr,"Usage: %s infile outfile"%sys.argv[0]
+	sys.exit(1)
+
+infile=sys.argv[1]
+outfile=sys.argv[2]
+
+if not os.path.isfile(infile):
+	print >>sys.stderr,"Please pass a valid input file"
+	sys.exit(1)
+
+if os.path.exists(outfile):
+	print >>sys.stderr,"Output file exists"
+	sys.exit(1)
+
+
+fh=open(infile,"r")
+ofh=open(outfile,"wb")
+
+out=""
+c=0
+num=0
+for a in fh.readlines():
+	for b in a:
+		if re.match('[A-Za-z]',b):
+			c=c+1
+			num=num*2
+			if re.match('[A-Z]',b):
+				num=num+1
+		if c==4:
+			d=re.sub('^0x','',hex(num))
+			out="%s%s"%(out,d)
+			num=0
+			c=0
+			
+
+ofh.write(binascii.a2b_hex(out))
